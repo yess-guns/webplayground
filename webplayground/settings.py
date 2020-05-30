2f3c32785b5c8a70e1d1a0a2be7a1ea9a0515b4a
yess-guns <yessy12384@gmail.com>
2020-05-30 00:38:29 -0500

    migrar a mysql

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'webplayground.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webplayground',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
