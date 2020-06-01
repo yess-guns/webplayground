d10e643cc656d81bd28b1b17dacf8c9fc76a08c7
yess-guns <yessy12384@gmail.com>
2020-05-31 21:41:00 -0500

    Implementado ListView en la app Pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PageListView#, PageDetailView, PageCreate, PageUpdate, PageDelete
 
 urlpatterns = [
-    path('', views.pages, name='pages'),
+    path('', PageListView.as_view(), name='pages'),
     path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
 ]
