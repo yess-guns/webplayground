f395ca9bf10a605a66a333e3c6fe5bd5499ca0ff
yess-guns <yessy12384@gmail.com>
2020-05-31 21:55:04 -0500

    usando detailview en la app pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from .views import PageListView#, PageDetailView, PageCreate, PageUpdate, PageDelete
+from .views import PageListView, PageDetailView#, PageCreate, PageUpdate, PageDelete
 
 urlpatterns = [
     path('', PageListView.as_view(), name='pages'),
-    path('<int:page_id>/<slug:page_slug>/', views.page, name='page'),
+    path('<int:pk>/<slug:slug>/', PageDetailView.as_view(), name='page'),
 ]
