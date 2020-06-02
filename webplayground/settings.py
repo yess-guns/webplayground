e15304f0e84be4867337641b7e1d7aef2d931f0f
yess-guns <yessy12384@gmail.com>
2020-06-02 01:05:06 -0500

    Creación de app registration

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Auth redirects
+LOGIN_REDIRECT_URL = 'pages:pages'
