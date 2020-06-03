115b2654dd17ba5cf58d6e82922df8840e5b1048
yess-guns <yessy12384@gmail.com>
2020-06-02 23:32:31 -0500

    restaurar contraseña

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -132,3 +132,11 @@ STATIC_URL = '/static/'
 # Auth redirects
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    # Aquí hay que configurar un email real para producción
+    pass
