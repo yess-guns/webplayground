a993b7e49b1d8cd63f0f8ab6583c607e1f6d2984
yess-guns <yessy12384@gmail.com>
2020-06-02 22:54:52 -0500

    Registro con CBV

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Paths de Auth
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('registration.urls')),
 ]
 
