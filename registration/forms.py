b2f3a217a31e755dd409feed4ec06ccb0914f938
yess-guns <yessy12384@gmail.com>
2020-06-03 00:59:08 -0500

    Mejorando formulario de perfil

--- registration/forms.py
+++ registration/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
+
 
 class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True, help_text="Requerido. 254 carácteres como máximo y debe ser válido.")
@@ -15,4 +17,13 @@ class UserCreationFormWithEmail(UserCreationForm):
             raise forms.ValidationError("El email ya está registrado, prueba con otro.")
         return email
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio', 'link']
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
+            'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
+            'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
+        }
 
