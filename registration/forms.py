5487e253cf0fbadd5cce38164825eb8993f48342
yess-guns <yessy12384@gmail.com>
2020-06-05 22:20:49 -0500

    editar correo y contraseña

--- registration/forms.py
+++ registration/forms.py
@@ -27,3 +27,16 @@ class ProfileForm(forms.ModelForm):
             'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
         }
 
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido. 254 carácteres como máximo y debe ser válido.")
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya está registrado, prueba con otro.")
+        return email
