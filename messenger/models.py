cbeba84438024b5ef0e9ddd656699d4b2ab87f6c
yess-guns <yessy12384@gmail.com>
2020-06-05 23:30:54 -0500

    refactorización

--- messenger/models.py
+++ messenger/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 class Message(models.Model):
@@ -14,4 +14,23 @@ class Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop("instance", None)
+    action = kwargs.pop("action", None)
+    pk_set = kwargs.pop("pk_set", None)
+    print(instance, action, pk_set)
+
+    false_pk_set = set()
+    if action is "pre_add":
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                print("Ups, ({}) no forma parte del hilo".format(msg.user))
+                false_pk_set.add(msg_pk)
+
+    # Borramos de pk_set los mensajes que concuerdan con los de false_pk_set
+    pk_set.difference_update(false_pk_set)
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
     
